9c27a1aa03f95d2dba6ff516f7f44671842a5971
bwennin92 <bwennin92@gmail.com>
2023-02-05 18:15:37 -0800

    Putting together python projects

---
+++ trainingpython.py
@@ -0,0 +1,59 @@
+x = 5
+y = 10
+z = x + y
+print(z) # Output: 15
+
+# integers
+x = 5
+y = 10
+
+# floating point numbers
+a = 5.5
+b = 10.7
+
+# strings
+name = "John"
+
+# lists
+fruits = ["apple", "banana", "cherry"]
+
+# dictionaries
+person = {"name": "John", "age": 32, "city": "New York"}
+
+# arithmetic operators
+x = 5
+y = 10
+z = x + y # addition
+print(z) # Output: 15
+z = x - y # subtraction
+print(z) # Output: -5
+
+# comparison operators
+x = 5
+y = 10
+z = x == y # equality
+print(z) # Output: False
+z = x > y # greater than
+print(z) # Output: False
+
+# logical operators
+x = True
+y = False
+z = x and y # logical and
+print(z) # Output: False
+
+# if statement
+x = 5
+if x > 0:
+  print("x is positive")
+
+# for loop
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+  print(fruit)
+
+# while loop
+x = 0
+while x < 5:
+  print(x)
+  x = x + 1
